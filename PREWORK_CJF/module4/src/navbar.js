be7b580cfc80c6084c76e2bfbc1296e11b1d62ff
Chris Flick <chrisflick@gmail.com>
2019-07-18 13:12:54 -0700

    Navbar now shows active page

--- PREWORK_CJF/module4/src/navbar.js
+++
@@ -1,5 +0,0 @@
-let home = "<a href='index.html'>Home</a>";
-let discog = "<a href='discography.html'>Discography</a>";
-let concert= "<a href='concert.html'>Concert</a>";
-
-document.getElementById("navbar").innerHTML = `${home} ${discog} ${concert}`;
