f901996253d372abd85bddb262ebee3810c8690a
Chris Flick <chrisflick@gmail.com>
2019-09-20 11:25:56 -0700

    Week 2 day 5 lesson 25 complete

--- 02 Week/05 Day/Activities/25-Stu_Render-Todos/script.js
+++ 02 Week/05 Day/Activities/25-Stu_Render-Todos/script.js
@@ -4,3 +4,16 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+renderTodo();
+
+
+function renderTodo(){
+    todoCountSpan.textContent = todos.length;
+
+    for (let i = 0; i < todos.length; i++) {
+        let li = document.createElement('li');
+        todoList.appendChild(li);
+        li.textContent = todos[i];
+    }
+}
