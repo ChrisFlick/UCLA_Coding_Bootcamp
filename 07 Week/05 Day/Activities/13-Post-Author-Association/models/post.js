b48a96271e95975c1463b018071cb597ef7f8508
Chris Flick <chrisflick@gmail.com>
2019-10-25 12:29:09 -0700

    completed lesso 13

--- 07 Week/05 Day/Activities/13-Post-Author-Association/models/post.js
+++ 07 Week/05 Day/Activities/13-Post-Author-Association/models/post.js
@@ -14,7 +14,16 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-  // Add a belongsTo association to Authors here
-  // Example: https://github.com/sequelize/express-example/blob/master/models/task.js
-  return Post;
+  Post.associate = function (model) {
+    // Add a belongsTo association to Authors here
+    model.Post.belongsTo(model.Author, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    // Example: https://github.com/sequelize/express-example/blob/master/models/task.js
+
+  }
+   return Post;
 };
